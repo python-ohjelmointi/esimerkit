57b5b4ef4bd65bda8cfa011a00b711673556c0f2
Teemu H <swd1tn002@users.noreply.github.com>
2022-01-23 12:22:45 +0200

    Ensimmäisen viikon esimerkkikoodit

--- osa01/latausaika.py
+++ osa01/latausaika.py
@@ -1,3 +1,13 @@
+'''
+Tämä esimerkkiskripti kysyy tiedoston koon ja liittymän nopeuden, ja antaa
+arvion tiedoston lataamiseen kuluvasta ajasta.
+
+Arvion saamiseksi ohjelmassa huomioidaan tiedostojen koon ja liittymien
+nopeuksien ilmoittamisessa olevat eroavaisuudet, eli kilo- ja mega-
+yksiköiden eri merkitykset. Lisäksi lasketaan tiedoston koko tavujen
+sijaan bitteinä.
+'''
+
 # megatavua  (mega = 1024**2)
 tiedosto_mb = int(input('Syötä tiedoston koko (Mt): '))
 koko_bitteina = tiedosto_mb * 1_024**2 * 8  # 1 tavu = 8 bittiä
