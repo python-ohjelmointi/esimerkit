9156fc3909bc908dd692fab731d4af6ef7a5a1b1
Teemu H <swd1tn002@users.noreply.github.com>
2022-01-31 15:52:23 +0200

    Lisätty teratavuja suuremmat yksiköt

--- osa01/tiedoston_koko_oneline.py
+++ osa01/tiedoston_koko_oneline.py
@@ -3,19 +3,19 @@ Tämä skripti on vaihtoehtoinen ratkaisu tiedoston_koko.py-skriptille.
 Vertailujen sijaan tässä ratkaisussa selvitetään ensin tiedoston kokoluokka
 luvun 1024 kertoimina logaritmin avulla, jossa X = log(tavut, 1_024).
 
-Tämän jälkeen koko jaetaan löydetyllä kokoluokalla 1024**X. Koon pääte (B, KB, MB..)
-valitaan listalta, jossa kunkin päätteen indeksi on sen suuruutta vastaava luvun
-1024 potenssi.
+Tämän jälkeen koko jaetaan löydetyllä kokoluokalla 1024**X. Koon yksikkö (B, KB, MB..)
+valitaan listalta, jossa kunkin yksikön indeksi sitä vastaava luvun 1024 potenssi.
 '''
 
 from math import log
 
-kertoimet = ['B', 'KB', 'MB', 'GB', 'TB']
+yksikot = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
 tavut = int(input('Syötä tiedoston koko tavuina: '))
 
 # kuinka monta kertaa 1024 pitää kertoa itsellään, jotta saadaan annetut tavut?
 potenssi = int(log(tavut, 1_024))
-koko = tavut / (1_024 ** potenssi)
+koko = tavut / (1_024**potenssi)
+yksikko = yksikot[potenssi]
 
-print(f'Tiedoston koko on {koko:.2f} {kertoimet[potenssi]}')
+print(f'Tiedoston koko on {koko:.2f} {yksikko}')
