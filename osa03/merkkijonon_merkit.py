cf7d051737fefec4e16fc8ec3bbc2310d8a0abca
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-03-04 21:35:01 +0200

    Käyttöehdot lisätty

--- osa03/merkkijonon_merkit.py
+++ osa03/merkkijonon_merkit.py
@@ -1,3 +1,9 @@
+'''
+Saat tutkia ja muokata tätä esimerkkikoodia harjoitustehtävän idean ymmärtämiseksi
+ja asioiden opettelemiseksi, mutta koodin kopiointi ja lähettäminen omissa nimissä
+on kielletty.
+'''
+
 mjono = input("Anna merkkijono: ")
 
 for kirjain in mjono:
