e8a87f933fb04ba1983585c73b562288999b3ea0
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-04-08 10:06:25 +0300

    Oppitunti 8.4.

---
+++ osa05/arvosanat.py
@@ -0,0 +1,8 @@
+'''
+Toteuta funktio arvosanojen_keskiarvo, joka saa parametrinaan listan kokonaislukuja,
+ja palauttaa niiden keskiarvon. Erikoisuutena arvosanojen keskiarvoja laskettaessa
+nollat tulee jättää huomioimatta.
+
+>>> arvosanojen_keskiarvo([0, 1, 0, 2, 0, 3])
+2.0
+'''
---
+++ osa05/fahrenheit.py
@@ -0,0 +1,13 @@
+'''
+Kirjoita funktio muunna_fahrenheitiksi, joka muuntaa sille annetun lämpötilan
+Celsius-asteikosta Fahrenheit-asteikkoon, ja palauttaa tuloksen.
+
+Muunnoskaava Celsius-asteikosta Fahrenheiteiksi on seuraava:
+
+°F = (°C) * 1,8 + 32
+
+Annettu lämpötila tulee siis kertoa luvulla 1,8 ja tulokseen tulee lisätä 32.
+
+>>> muunna_fahrenheitiksi(55)
+131.0
+'''
